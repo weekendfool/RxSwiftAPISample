4f45bfe533c2d7065840b56310a6dfcec6c954b5
尾原徳泰 <nori.tukareta@gmail.com>
2022-06-13 19:25:44 +0900

    RxSwift RxCocoa インストール

---
+++ RxSwiftAPISample/Pods/Target Support Files/Pods-RxSwiftAPISample-RxSwiftAPISampleUITests/Pods-RxSwiftAPISample-RxSwiftAPISampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftAPISample_RxSwiftAPISampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftAPISample_RxSwiftAPISampleUITests
+@end
---
+++ RxSwiftAPISample/RxSwiftAPISample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftAPISample
+//
+//  Created by Oh!ara on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
